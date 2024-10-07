30632da944d0fc8fb89e092977a107786bab0e51
theDanteAesthetic <dante.squires@sonomaacademy.org>
2024-10-07 10:31:44 -0700

    commit p2

--- multiset_files/challenge1.cpp
+++ multiset_files/challenge1.cpp
@@ -4,18 +4,18 @@
 // Constructor implementation
 Challenge1::Challenge1(const multiset<int>& nums) {
     // Initializes the multiset with given numbers
-    add_your_code_here
+    numbers = nums;
 }
 
 // Method to insert a number
 void Challenge1::insertNumber(int num) {
-    numbers.insert(add_your_code); // Insert the number into the multiset
-    multiset<int>::iterator begin = add_your_code_here;
-    multiset<int>::iterator end = add_your_code_here;
+    numbers.insert(num); // Insert the number into the multiset
+    multiset<int>::iterator begin = numbers.begin();
+    multiset<int>::iterator end = numbers.end();
     cout << "Challenge 1" << endl;
     cout << "Print the multiset with the new value inserted " <<endl;
     for (multiset<int>::iterator it = begin; it != end; ++it) {
-        cout << *add_your_code_here << " ";
+        cout << *it << " ";
     }
     cout << endl;
 }
