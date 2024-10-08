f1ec5b93176854de9af1d67e57f9595101beaf4f
theDanteAesthetic <dante.squires@sonomaacademy.org>
2024-10-07 19:25:11 -0700

    test code of Caesar Cipher using classes

--- CaeserClass/Caesar.cpp
+++ CaeserClass/Caesar.cpp
@@ -1,4 +1,5 @@
 #include "Caesar.h"
+using namespace std;
 
 static const int maxchar = 'z';
 static const int minchar = ' ';
@@ -6,16 +7,50 @@ static const int charrange = 1 + maxchar - minchar;
 
 // Caesar constructor, accepts a single string argument as the cipher
 // key
-
+Caesar::Caesar(string k){
+    key = k;
+}
 
 // get_key: return the current cipher key
-
+string Caesar::get_key() const{
+    return key;
+}
 
 //encode: encode the string argument using the key, return encoded string
-
+string Caesar::encode(string message) const{
+    string result = "";
+    for(int i = 0; i < message.size(); i++){
+        char messageChar = message[i];
+        char keyChar = key[i % key.size()];
+        int messageIndex = (int)messageChar - (int)minchar;
+        int keyIndex = (int)keyChar - (int)minchar;
+        int encryptedIndex = (messageIndex + keyIndex) % charrange;
+        char encryptedChar = static_cast<char>(encryptedIndex + (int)minchar);
+        result += encryptedChar;
+    }
+    return result;
+}
 
 //decode: decode the string argument using the key, return decoded string
-
+string Caesar::decode(string message) const{
+    string result = "";
+    for(int i = 0; i < message.size(); i++){
+        char messageChar = message[i];
+        char keyChar = key[i % key.size()];
+        int messageIndex = (int)messageChar;
+        int keyIndex = (int)keyChar;
+        int encryptedIndex = safemod((messageIndex - keyIndex), charrange);
+        char encryptedChar = static_cast<char>(encryptedIndex);
+        result += encryptedChar;
+    }
+    return result;
+}
 
 //A mod function that is "safe" to use even when val might be negative
-
+char Caesar::safemod(int val, int mod) const{
+    int modval = val % mod;
+    if (modval < 0) {
+        modval += mod;
+    }
+    return char(modval + 32);
+}
--- CaeserClass/caesar_main.cpp
+++ CaeserClass/caesar_main.cpp
@@ -3,3 +3,36 @@
 // previous assignment here if you think it would be useful, but the 
 // current application MUST use the new object-oriented library interface 
 // to do encoding and decoding.
+#include <iostream>
+#include "Caesar.h"
+#include "Caesar.cpp"
+using namespace std;
+
+int main(){
+    string message, key, answer;
+    cout << "Would you like to encode or decode a message?" << endl;
+    cin >> answer;
+
+    if(answer != ""){
+        if(answer != "encode" && answer != "decode"){
+            cout << "Sorry! I didn't quite catch that! Please answer either 'encode' or 'decode'" << endl;
+            cout << "Would you like to encode or decode a message?" << endl;
+            cin >> answer;
+        }
+        cout << "What message would you like to " << answer << endl;
+        cin >> message; //I tried using getline() so it didn't ignore whitespace but it didn't work
+        if(answer == "encode"){
+            cout << "What encryption key would you like to use?" << endl;
+            cin >> key;
+            Caesar Caesar(key);
+            cout << "encoded message: " << Caesar.encode(message) << endl;
+        }
+        if(answer == "decode"){
+            cout << "What decryption key would you like to use?" << endl;
+            Caesar Caesar(key);
+            cout << "decoded message: " << Caesar.decode(message) << endl;
+        }
+        }
+    
+    
+}
